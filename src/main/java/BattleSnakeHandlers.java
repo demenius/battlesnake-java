24333e9e990e118eeccd42fc49e27f1587af0c4a
Demenius <smog0@hotmail.com>
2015-02-23 13:36:22 -0800

    Narrow Valid Moves
    Created a function that would narrow down valid moves based on how much
    of the map would be cut off by moving a certain direction.

--- src/main/java/BattleSnakeHandlers.java
+++ src/main/java/BattleSnakeHandlers.java
@@ -10,7 +10,8 @@ import java.util.Queue;
 public class BattleSnakeHandlers
 {
 
-    public static String NAME = "Inland-Taipans";
+    public static final String NAME = "Inland-Taipans";
+    public static final double MAX_VALID_CUTOFF = 0.90f;
 
     public Object handleStart(Map<String, Object> requestBody)
     {
@@ -59,14 +60,85 @@ public class BattleSnakeHandlers
 
     private String getMove()
     {
-        calculateShortestDistanceMap();
         narrowValidMoves();
+        calculateShortestDistanceMap();
+        
         return foodDirection();
     }
     
+    /**
+     * Narrows down valid moves based on how much of the map the snake would cut off by moving a certain direction
+     */
     private void narrowValidMoves()
     {
+        int leftValid = 0;
+        int rightValid = 0;
+        int upValid = 0;
+        int downValid = 0;
         
+        if(!reverseDir().equals("left"))
+        {
+            calculateShortestDistanceMap(ourNextCoords("left"), true);
+            leftValid = getValidMoves();
+        } else if(!reverseDir().equals("right"))
+        {
+            calculateShortestDistanceMap(ourNextCoords("right"), true);
+            rightValid = getValidMoves();
+        } else if(!reverseDir().equals("up"))
+        {
+            calculateShortestDistanceMap(ourNextCoords("up"), true);
+            upValid = getValidMoves();
+        } else
+        {
+            calculateShortestDistanceMap(ourNextCoords("down"), true);
+            downValid = getValidMoves();
+        }
+        
+        double maxValid = getMaxValidMoves() * MAX_VALID_CUTOFF;
+        if(leftValid < maxValid)
+            Board.invalidCoords.add(ourNextCoords("left"));
+        if(rightValid < maxValid)
+            Board.invalidCoords.add(ourNextCoords("right"));
+        if(upValid < maxValid)
+            Board.invalidCoords.add(ourNextCoords("up"));
+        if(downValid < maxValid)
+            Board.invalidCoords.add(ourNextCoords("down"));
+    }
+    
+    /**
+     * @return The number of open positions on the map that you can reach
+     */
+    private int getValidMoves()
+    {
+        int valid = 0;
+        int max = Board.width * Board.height;
+        for(int i = 0; i < Board.distanceMap.length; i++)
+        {
+            for(int j = 0; j < Board.distanceMap[0].length; j++)
+            {
+                if(Board.distanceMap[i][j] != -1 || Board.distanceMap[i][j] != max)
+                    valid++;
+            }
+        }
+        return valid;
+    }
+    
+    /**
+     * @return The number of open positions on the map
+     */
+    private int getMaxValidMoves()
+    {
+        int valid = 0;
+        int max = Board.width * Board.height;
+        for(int i = 0; i < Board.distanceMap.length; i++)
+        {
+            for(int j = 0; j < Board.distanceMap[0].length; j++)
+            {
+                if(Board.distanceMap[i][j] != -1)
+                    valid++;
+            }
+        }
+        return valid;
     }
 
     private String foodDirection()
@@ -94,22 +166,25 @@ public class BattleSnakeHandlers
     }
     
     
-
+    /**
+     * @param c The coordinates to check. Must be a neighbouring coordinate
+     * @return The direction in which the coordinates are coming from
+     */
     private String coordToDir(int[] c)
     {
-        if (Arrays.equals(getNextCoords("left"), c))
+        if (Arrays.equals(ourNextCoords("left"), c))
         {
             return "left";
         }
-        if (Arrays.equals(getNextCoords("right"), c))
+        if (Arrays.equals(ourNextCoords("right"), c))
         {
             return "right";
         }
-        if (Arrays.equals(getNextCoords("up"), c))
+        if (Arrays.equals(ourNextCoords("up"), c))
         {
             return "up";
         }
-        if (Arrays.equals(getNextCoords("down"), c))
+        if (Arrays.equals(ourNextCoords("down"), c))
         {
             return "down";
         }
@@ -117,6 +192,13 @@ public class BattleSnakeHandlers
 
     }
 
+    /**
+     * @param x1 The x coordinate of the first location
+     * @param y1 The y coordinate of the first location
+     * @param x2 The x coordinate of the second location
+     * @param y2 The y coordinate of the second location
+     * @return -1 If (x1,y1) and (x2,y2) are invalid. 0 If the distance to (x1,y1) < (x2,y2) else 1
+     */
     private int whichOne(int x1, int y1, int x2, int y2)
     {
         if (checkXY(x1, y1))
@@ -136,30 +218,51 @@ public class BattleSnakeHandlers
         return -1;
     }
 
+    /**
+     * @param x x Coordinate to check
+     * @param y y Coordinate to check
+     * @return true if (x,y) is on the map and can be reached from the current location
+     */
     private boolean checkXY(int x, int y)
     {
         return validX(x) && validY(y) && Board.distanceMap[x][y] != -1 && Board.distanceMap[x][y] != (Board.width * Board.height);
     }
 
+    /**
+     * @param x x Coordinate to check
+     * @return true if x is on the map
+     */
     private boolean validX(int x)
     {
         return (x >= 0 && x < Board.width);
     }
 
+    /**
+     * @param y y Coordinate to check
+     * @return true if y is on the map
+     */
     private boolean validY(int y)
     {
         return (y >= 0 && y < Board.height);
     }
 
+    /**
+     * @param x x Coordinate to check
+     * @param y y Coordinate to check
+     * @return true if (x,y) is the head of our snake
+     */
     private boolean isHead(int x, int y)
     {
         return (ourCoords()[0] == x && ourCoords()[1] == y);
     }
 
-    // Mov = 0 : x-1
-    // Mov = 1 : x+1
-    // Mov = 2 : y-1
-    // Mov = 3 : y+1
+    // 
+    /**
+     * @param x x Coordinate to check
+     * @param y y Coordinate to check
+     * @param lastMov Last action that was taken 0: x-1, 1: x+1, 2: y-1, 3: y+1
+     * @return The first coordinates for the shortest path to (x,y)
+     */
     private int[] shortestPath(int x, int y, int lastMov)
     {
         int x0VSx1 = whichOne(x - 1, y, x + 1, y);
@@ -240,6 +343,9 @@ public class BattleSnakeHandlers
         return t;
     }
 
+    /**
+     * @return The closest food coordinates
+     */
     private int[] findShortestFoodCoord()
     {
         int shortest = Board.width * Board.height;
@@ -258,12 +364,31 @@ public class BattleSnakeHandlers
         return coord;
     }
 
+    /**
+     * Creates a shortest distance map
+     */
     private void calculateShortestDistanceMap()
     {
-        Board.distanceMap[ourCoords()[0]][ourCoords()[1]] = 0;
+        calculateShortestDistanceMap(ourCoords(), false);
+    }
+    
+    /**
+     * Creates a shortest distance map with the head in a custom position
+     * @param head The coordinates for the custom head position
+     */
+    private void calculateShortestDistanceMap(int[] head, boolean removeTail)
+    {
+        resetDistanceMap();
+        Board.distanceMap[head[0]][head[1]] = 0;
+        if(removeTail)
+        {
+            int[] tail = ours().coords[ours().coords.length-1];
+            Board.distanceMap[tail[0]][tail[1]] = Board.width * Board.height;
+        }
         calcShortDist(ourCoords()[0], ourCoords()[1]);
     }
 
+    
     private void calcShortDist(int x, int y)
     {
         calcShortHelper(x, y, x - 1, y);
@@ -286,24 +411,24 @@ public class BattleSnakeHandlers
         }
     }
 
-    private int[] getNextCoords(String dir)
+    private int[] ourNextCoords(String dir)
     {
         if (dir.equals("left"))
         {
-            return ourCoords(-1, 0);
+            return ourNextCoords(-1, 0);
         }
         if (dir.equals("right"))
         {
-            return ourCoords(1, 0);
+            return ourNextCoords(1, 0);
         }
         if (dir.equals("up"))
         {
-            return ourCoords(0, -1);
+            return ourNextCoords(0, -1);
         }
-        return ourCoords(0, 1);
+        return ourNextCoords(0, 1);
     }
 
-    private int[] ourCoords(int x, int y)
+    private int[] ourNextCoords(int x, int y)
     {
         int[] c = ourCoords().clone();
         c[0] += x;
@@ -390,16 +515,7 @@ public class BattleSnakeHandlers
         parseSnakes((ArrayList<Map<String, Object>>) requestBody.get("snakes"));
         Board.food = toDoubleIntArray((ArrayList<ArrayList<Integer>>) requestBody.get("food"));
         
-        Board.validMoves = new ArrayList<String>();
-        Board.validMoves.add("left");
-        Board.validMoves.add("right");
-        Board.validMoves.add("up");
-        Board.validMoves.add("down");
-        if(Board.turn != 0)
-        {
-            Board.validMoves.remove(reverseDir());
-        }
-        
+        Board.invalidCoords.clear();
     }
 
     private void parseBoardTiles(ArrayList<ArrayList<Object>> tiles)
@@ -411,13 +527,6 @@ public class BattleSnakeHandlers
             {
                 Map<String, String> q = (Map<String, String>) tiles.get(i).get(j);
                 ((BoardTile) Board.board[i][j]).state = BoardTile.State.getState(q.get("state"));
-                if (Board.board[i][j].state == BoardTile.State.BODY || Board.board[i][j].state == BoardTile.State.HEAD)
-                {
-                    Board.distanceMap[i][j] = -1;
-                } else
-                {
-                    Board.distanceMap[i][j] = Board.width * Board.height;
-                }
             }
         }
     }
@@ -429,7 +538,7 @@ public class BattleSnakeHandlers
         {
             for (int j = 0; j < Board.height; j++)
             {
-                if (Board.board[i][j].state == BoardTile.State.BODY || Board.board[i][j].state == BoardTile.State.HEAD)
+                if (Board.board[i][j].state == BoardTile.State.BODY || Board.board[i][j].state == BoardTile.State.HEAD || Board.invalidCoords.contains(toIntArray(i, j)))
                 {
                     Board.distanceMap[i][j] = -1;
                 } else
@@ -484,7 +593,8 @@ public class BattleSnakeHandlers
 
     private static class Board
     {
-        public static ArrayList<String> validMoves;
+        public static int[][] distanceMap;
+        public static ArrayList<int[]> invalidCoords = new ArrayList<int[]>();
 
         public static int width;
         public static int height;
@@ -492,7 +602,6 @@ public class BattleSnakeHandlers
         public static String game_id;
         public static int turn;
         public static BoardTile[][] board;
-        public static int[][] distanceMap;
         public static int[][] checkMap;
         public static Map<String, Snake> snakes = new HashMap<String, Snake>();
         public static int[][] food;
